3c4d7d3714139b9bcb73ac584e8810164dd6ba6e
Mykola Pererbenesiuk <sanistray@gmail.com>
2021-12-18 12:18:55 +0200

    changes

---
+++ js/homepage_script.js
@@ -0,0 +1,230 @@
+var trackStep = $('.map__radius_track').width();
+
+$('.faq__item').click(function() {
+    console.log('5')
+    var currentFaq = $(this)
+    currentFaq.find('.faq__item_answer').toggleClass('faq_ans_open')
+    currentFaq.toggleClass('faq_open');
+
+    if (currentFaq.hasClass('faq_open')) {
+        $(this).find('.faq__open').attr('src', 'img/homepage/faq_minus.svg')
+    } else {
+        $(this).find('.faq__open').attr('src', 'img/homepage/faq_plus.svg')
+    }
+})
+
+$('.current__map_buttons button').click(function() {
+    $('.current__map_buttons button').removeClass('current__button_active')
+    $(this).addClass('current__button_active')
+})
+
+var sliderNumber = 1;
+$('.review__slider_dot').removeClass('slider__dot_active')
+$('#slider_dot_' + sliderNumber).addClass('slider__dot_active')
+setInterval(() => {
+    var sliderCount = $('.reviews__image_wrapper img').length;
+    var sliderWidth = $('.reviews__image_wrapper img').width();
+    var sliderContainer = $('.reviews__image_wrapper')
+
+    $('.review__slider_dot').removeClass('slider__dot_active')
+    $('#slider_dot_' + sliderNumber).addClass('slider__dot_active')
+
+    sliderNumber++;
+
+    var translateWidth = (sliderNumber - 1) * sliderWidth;
+    console.log(sliderCount)
+    if (sliderNumber > sliderCount) {
+        sliderNumber = 1;
+        sliderContainer.attr('style', 'transform: translateX(-' + 0 + 'px)')
+    } else {
+        sliderContainer.attr('style', 'transform: translateX(-' + translateWidth + 'px)')
+    }
+
+}, 3000);
+
+/* GOOGLE MAP */
+
+// This example requires the Places library. Include the libraries=places + AIzaSyDFA6udGwPSgJt_QacZp9tatCTazR32t2U
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&libraries=places">
+
+$(".map__radius_draggable").draggable({
+    containment: "parent",
+    axis: "x",
+    drag: function(e, ui) {
+        x2 = ui.position.left;
+        var trackPercent = (x2 * 100) / trackStep
+        if (trackPercent > 90) {
+            $('.map__radius_draggable').text('7 km')
+            mapRadius = 9;
+        } else if (trackPercent > 75) {
+            $('.map__radius_draggable').text('6 km')
+            mapRadius = 8;
+        } else if (trackPercent > 60) {
+            $('.map__radius_draggable').text('5 km')
+            mapRadius = 7;
+        } else if (trackPercent > 45) {
+            $('.map__radius_draggable').text('4 km')
+            mapRadius = 6;
+        } else if (trackPercent > 30) {
+            $('.map__radius_draggable').text('3 km')
+            mapRadius = 5;
+        } else if (trackPercent > 15) {
+            $('.map__radius_draggable').text('2 km')
+            mapRadius = 4;
+        } else if (trackPercent < 15) {
+            $('.map__radius_draggable').text('1 km')
+            mapRadius = 3;
+        }
+    }
+});
+
+var myLatlng;
+var userEmail;
+var catAdress;
+
+function initMap() {
+    const componentForm = [
+        'location',
+        'locality',
+        'administrative_area_level_1',
+        'country',
+        'postal_code',
+    ];
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 17,
+        center: { lat: 37.4221, lng: -122.0841 },
+        mapTypeControl: false,
+        mapTypeId: "terrain",
+        fullscreenControl: false,
+        streetViewControl: false
+    });
+    const map2 = new google.maps.Map(document.getElementById("current_map"), {
+        zoom: 14,
+        center: { lat: 37.4221, lng: -122.0841 },
+        mapTypeControl: false,
+        mapTypeId: "terrain",
+        fullscreenControl: false,
+        streetViewControl: false,
+        disableDefaultUI: true
+    });
+
+    image = 'img/icons/marker.svg'
+    const marker = new google.maps.Marker({ map: map, draggable: false, icon: image });
+    const autocompleteInput = document.getElementById('location');
+    const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+        fields: ["address_components", "geometry", "name"],
+        types: ["address"],
+
+    });
+
+    var radiusOnMap;
+
+    $('#location').keypress(function(e) {
+        if (e.which == 13) {
+            adressSelect()
+            return false; //prevent duplicate submission
+        }
+    });
+
+    function adressSelect() {
+        marker.setVisible(false);
+        const place = autocomplete.getPlace();
+        catAdress = place.name;
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert('No details available for input: \'' + place.name + '\'');
+            return;
+        }
+        renderAddress(place);
+        fillInAddress(place);
+
+        var mapRadius = 2;
+
+        var markersArray = [];
+        markersArray.push(
+            [
+                place.name, {
+                    center: place.geometry.location,
+                    population: mapRadius,
+                }
+            ]
+        )
+        const cityCircle = new google.maps.Circle({
+
+        });
+        if (radiusOnMap == true) {
+            cityCircle.setMap(null);
+            radiusOnMap = true;
+        } else {
+            const cityCircle = new google.maps.Circle({
+                strokeColor: "#F8A35B",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#F8A35B",
+                fillOpacity: 0.3,
+                map,
+                center: markersArray[0][1].center,
+                radius: Math.sqrt(mapRadius) * 100,
+            });
+            radiusOnMap = true;
+            console.log(radiusOnMap)
+        }
+
+        mapInput()
+
+        function mapInput() {
+            $('#map').removeClass('map__blured');
+            $('.search__map_button').attr('style', 'border-top-right-radius: 0px')
+            $('.search__map_form').addClass('form_in_top')
+            $('.search__map_button').text('Aanmelden')
+            $('.search__map_button').attr('id', 'map__button_top')
+
+            document.cookie = "steps=2";
+            document.cookie = "adress=" + catAdress;
+            //    alert(document.cookie)
+            document.location.href = "/search.html";
+        }
+    }
+    $('.search__map_button').click(function() {
+        adressSelect()
+    })
+
+    function fillInAddress(place) { // optional parameter
+        const addressNameFormat = {
+            'street_number': 'short_name',
+            'route': 'long_name',
+            'locality': 'long_name',
+            'administrative_area_level_1': 'short_name',
+            'country': 'long_name',
+            'postal_code': 'short_name',
+        };
+        const getAddressComp = function(type) {
+            for (const component of place.address_components) {
+                if (component.types[0] === type) {
+
+                    return component[addressNameFormat[type]];
+                }
+            }
+            return '';
+        };
+        document.getElementById('location').value = getAddressComp('street_number') + ' ' +
+            getAddressComp('route');
+        for (const component of componentForm) {
+            // Location field is handled separately above as it has different logic.
+            if (component !== 'location') {
+                document.getElementById(component).value = getAddressComp(component);
+            }
+        }
+    }
+
+    function renderAddress(place) {
+        map.setCenter(place.geometry.location);
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+    }
+}
+
+/* GOOGLE MAP END */
--- js/main.js
+++ js/main.js
@@ -1,15 +1,126 @@
+/* COOKIE DATA  */
+console.log(document.cookie)
+
+var str = document.cookie;
+
+var cookieArray = str.split(/[;] */).reduce(function(result, pairStr) {
+    var arr = pairStr.split('=');
+    if (arr.length === 2) { result[arr[0]] = arr[1]; }
+    return result;
+}, {});
+
+
+console.log(cookieArray)
+
+var chatMessageArray = [];
+
+if (typeof cookieArray.steps != 'undefined') {
+    var stepContainer = $('.search__steps_container');
+    var scrollWidth = $('.search__steps_container > div').width();
+    var calculatedTranslate = (cookieArray.steps - 1) * scrollWidth;
+    stepContainer.attr('style', 'transform: translateX(-' + calculatedTranslate + 'px)')
+    $('.search__chat > h1 > span').text(cookieArray.adress)
+    $('#step-1 > .search__step_circle').addClass('step__green')
+    $('#step-2 > .search__step_circle').addClass('step__active')
+
+    $('.chat__left_start').attr('style', 'display: flex')
+    $('#bot_msg_0').attr('style', 'display: flex')
+    $('.chat__start_repeat').click(function() {
+        $('#user_msg_1').attr('style', 'display:flex')
+        $('#user_msg_1').addClass('user__msg_show');
+        setTimeout(() => {
+            $('.chat__left_input').attr('style', 'display:block');
+            $('#bot_msg_1').attr('style', 'display:flex');
+            $('#bot_msg_1').addClass('chat__msg_bot')
+            $('.chat__left_start').attr('style', 'display:none')
+
+        }, 500);
+    })
+    $('.chat__start_where').click(function() {
+        chatHistoryBuild()
+    })
+}
+
+function chatHistoryBuild() {
+    $.each(cookieArray, function(key, value) {
+        console.log(cookieArray)
+        if (typeof cookieArray.catName != 'undefined') {
+            $('.chat__left_start').attr('style', 'display: none')
+            $('.cat__name_chat').attr('style', 'display: flex').addClass('user__msg_show')
+            $('#user_msg_2').find('p').text(cookieArray.catName)
+            $('.chat__catname').attr('style', 'opacity:1')
+            $('.chat__see_cat').attr('style', 'opacity:1')
+            $('.chat__catname > span').text(cookieArray.catName)
+            $('.chat__see_cat span').text(cookieArray.catName)
+            $('.chat__cat_desc > span').text(cookieArray.adress)
+            $('.chat__last_loc > span').text(cookieArray.adress)
+            $('.chat__cat_desc').html('<span>kattenradar </span>' + cookieArray.catDesc)
+
+            if (typeof cookieArray.catImage != 'undefined') {
+                $('#user_msg_3').attr('style', 'display: none').removeClass('user__msg_show')
+                $('.cat__img_chat').attr('style', 'display: flex').addClass('user__msg_show');
+                $('.chat__phone__cat').attr('src', cookieArray.catImage);
+                $('.chat__phone__cat').attr('style', 'display: flex')
+
+
+                $('.chat__phone__cat').attr('src', cookieArray.catImage)
+                $('.chat__phone__cat').attr('display', 'block')
+
+                if (typeof cookieArray.userEmail != 'undefined') {
+                    $('.user_email_chat').attr('style', 'display: flex').addClass('user__msg_show');
+                    $('#user_msg_6').find('p').text(cookieArray.userEmail);
+
+                    if (typeof cookieArray.userNumber != 'undefined') {
+                        $('.user_number_chat').attr('style', 'display: flex').addClass('user__msg_show');
+                        $('#bot_msg_9').find('p > span').text(cookieArray.userNumber)
+                        $('#bot_msg_10').addClass('user__msg_show')
+                    } else {
+                        $('.chat__left_buttons_email').attr('style', 'display: flex')
+                    }
+                } else {
+                    $('.chat__left_input_email').attr('style', 'display: block')
+                }
+            }
+        } else {
+
+            $('#bot_msg_1').attr('style', 'display: flex');
+            $('#bot_msg_1').addClass('chat__msg_bot');
+            $('.chat__left_input').attr('style', 'display:flex');
+            return false;
+        }
+    })
+}
+
+
 /* SEARCH */
 
-$('.search__step_info').hover(
+$('#map__button_top').click(function() {
+    if ($('.search__map_form').hasClass('form_in_top')) {
+        $('.search__steps').attr('style', 'background-image: url(./img/step__line_2.svg)')
+        $('#step-1').find('.search__step_circle').addClass('step__done')
+
+        $('#step-2').removeClass('step_inactive')
+
+        $('.search__steps_container').attr('style', 'transform: translateX(-1110px)')
+
+        $('.chat__last_loc > span').text(catAdress)
+        document.cookie = "steps=2";
+        document.cookie = "adress=" + catAdress;
+
+        $('#bot_msg_1').attr('style', 'display: flex');
+        $('#bot_msg_1').addClass('chat__msg_bot');
+        $('.chat__left_input').attr('style', 'display:flex');
+    }
+})
+
+$('.search__step').hover(
     function() {
-        $(this).parents('.search__step').find('.search__step_desc').addClass('search__step_desc_show')
+        $(this).find('.search__step_desc').addClass('search__step_desc_show')
     },
     function() {
         $('.search__step_desc_show').removeClass('search__step_desc_show')
     }
 )
-
-
 
 /* GOOGLE MAP */
 
@@ -18,6 +129,8 @@ $('.search__step_info').hover(
 // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&libraries=places">
 
 var myLatlng;
+var userEmail;
+var catAdress;
 
 function initMap() {
     const componentForm = [
@@ -29,13 +142,15 @@ function initMap() {
     ];
 
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 18,
+        zoom: 17,
         center: { lat: 37.4221, lng: -122.0841 },
         mapTypeControl: false,
         mapTypeId: "terrain",
-        fullscreenControl: true,
-        zoomControl: true,
-        streetViewControl: true
+        fullscreenControl: false,
+        zoomControl: false,
+        draggable: false,
+        scrollwheel: false,
+        streetViewControl: false
     });
     image = 'img/icons/marker.svg'
     const marker = new google.maps.Marker({ map: map, draggable: false, icon: image });
@@ -46,9 +161,19 @@ function initMap() {
 
     });
 
-    $('.search__map_button').click(function() {
+    var radiusOnMap;
+
+    $('#location').keypress(function(e) {
+        if (e.which == 13) {
+            adressSelect()
+            return false; //prevent duplicate submission
+        }
+    });
+
+    function adressSelect() {
         marker.setVisible(false);
         const place = autocomplete.getPlace();
+        catAdress = place.name;
         if (!place.geometry) {
             // User entered the name of a Place that was not suggested and
             // pressed the Enter key, or the Place Details request failed.
@@ -58,17 +183,22 @@ function initMap() {
         renderAddress(place);
         fillInAddress(place);
 
+        var markersArray = [];
+        markersArray.push(
+            [
+                place.name, {
+                    center: place.geometry.location,
+                    population: 2,
+                }
+            ]
+        )
+        const cityCircle = new google.maps.Circle({
 
-        const citymap = {
-            marker: {
-                center: place.geometry.location,
-                population: 2,
-            }
-        };
-        console.log(place.geometry.location)
-
-        for (const city in citymap) {
-            // Add the circle for this city to the map.
+        });
+        if (radiusOnMap == true) {
+            cityCircle.setMap(null);
+            radiusOnMap = true;
+        } else {
             const cityCircle = new google.maps.Circle({
                 strokeColor: "#F8A35B",
                 strokeOpacity: 0.8,
@@ -76,9 +206,11 @@ function initMap() {
                 fillColor: "#F8A35B",
                 fillOpacity: 0.3,
                 map,
-                center: citymap[city].center,
-                radius: Math.sqrt(citymap[city].population) * 100,
+                center: markersArray[0][1].center,
+                radius: Math.sqrt(markersArray[0][1].population) * 100,
             });
+            radiusOnMap = true;
+            console.log(radiusOnMap)
         }
 
         mapInput()
@@ -87,7 +219,12 @@ function initMap() {
             $('#map').removeClass('map__blured');
             $('.search__map_button').attr('style', 'border-top-right-radius: 0px')
             $('.search__map_form').addClass('form_in_top')
+            $('.search__map_button').text('Aanmelden')
+            $('.search__map_button').attr('id', 'map__button_top')
         }
+    }
+    $('.search__map_button').click(function() {
+        adressSelect()
     })
 
     function fillInAddress(place) { // optional parameter
@@ -124,3 +261,298 @@ function initMap() {
         marker.setVisible(true);
     }
 }
+
+/* GOOGLE MAP END */
+
+/* STEPS LOGIC */
+
+/* STEPS SWITCHER */
+
+$('.search__step').click(function() {
+    var clickedStep = $(this).attr('id').replace('step-', '')
+    var stepContainer = $('.search__steps_container');
+    var scrollWidth = $('.search__steps_container > div').width();
+    var calculatedTranslate = (clickedStep - 1) * scrollWidth;
+    $('.search__chat > h1 > span').text(catAdress)
+
+    if (clickedStep == '1') {
+        stepContainer.attr('style', 'transform: translateX(-' + calculatedTranslate + 'px)')
+    } else if (clickedStep == '2') {
+        if (cookieArray.steps > 1)
+            stepContainer.attr('style', 'transform: translateX(-' + calculatedTranslate + 'px)')
+    } else if (clickedStep == '3') {
+        if (cookieArray.steps > 2)
+            stepContainer.attr('style', 'transform: translateX(-' + calculatedTranslate + 'px)')
+    } else if (clickedStep == '4') {
+        if (cookieArray.steps > 3)
+            stepContainer.attr('style', 'transform: translateX(-' + calculatedTranslate + 'px)')
+    }
+})
+
+/* STEPS 1 - 2  */
+var catName;
+
+
+$('#bot_msg_0').addClass('chat__msg_bot')
+
+
+$('.chat__cat_send').click(function() {
+    catName = $('#cat_name').val()
+    document.cookie = "catName=" + catName;
+    setTimeout(() => {
+        catNameSend()
+    }, 0);
+})
+
+function catNameSend() {
+    catName = $('#cat_name').val()
+    document.cookie = "catName=" + catName;
+
+    $('.chat__left_input').attr('style', 'display:none')
+
+    $('#user_msg_2').find('p').text(catName)
+    $('#user_msg_2').attr('style', 'display: flex')
+    $('#user_msg_2').addClass('user__msg_show');
+    $('.chat__catname > span').text(catName);
+    $('.chat__catname').css('opacity', '1')
+
+    $('.chat__see_cat > span').text(catName);
+    $('.chat__see_cat').css('opacity', '1')
+
+    setTimeout(() => {
+        $('#bot_msg_2').attr('style', 'display: flex')
+        $('#bot_msg_2').addClass('chat__msg_bot')
+        $('#user_msg_3').attr('style', 'display: flex')
+        $('#user_msg_3').addClass('user__msg_show');
+    }, 500);
+}
+
+$('#cat_name').keypress(function(e) {
+    if (e.which == 13) {
+        catNameSend()
+        return false; //prevent duplicate submission
+    }
+});
+
+$('.chat_file_upload').click(function() {
+    $('#user_msg_3').attr('style', 'display: none')
+    $('#user_msg_4').attr('style', 'display: flex')
+    var catImage = $('#user_msg_4').find('img').attr('src');
+    $('.chat__phone__cat').attr('src', catImage);
+    $('.chat__phone__cat').css('display', 'block')
+
+
+    $('#user_msg_4').addClass('user__msg_show');
+    setTimeout(() => {
+        $('#bot_msg_3').attr('style', 'display: flex')
+        $('#bot_msg_3').addClass('chat__msg_bot')
+        $('.chat__left_buttons').attr('style', 'display: flex')
+
+        document.cookie = "catImage=" + catImage;
+
+    }, 500);
+})
+$('#img__upload').click(function() {
+    $('#bot_msg_2').attr('style', 'display: none')
+    $('#bot_msg_2').removeClass('chat__msg_bot')
+    $('#user_msg_3').attr('style', 'display: none')
+    $('#user_msg_3').removeClass('user__msg_show');
+
+    $('#bot_msg_1').attr('style', 'display: flex')
+    $('.chat__left_input').attr('style', 'display:flex')
+})
+
+$('.chat__left_b_done').click(function() {
+    $('#user_msg_5_1').attr('display', 'flex')
+    $('#user_msg_5_1').addClass('user__msg_show');
+    $('.chat__left_buttons').attr('style', 'display:none')
+    $('.chat__input_big').attr('style', 'display:flex')
+    $('#cat_desc').val(`opgesloten in schuren of bij  gebou-wen. Erg mens vriendelijke kat makkelijk. Opgesloten in schuren of bij gebouwen. Erg mens vriendelijke kat makkelijk. Mogelijk opgesloten in schuren of bij  gebou-wen. Erg mens vriendelijke kat makkelijk. Opgesloten in schuren of bij gebouwen. Erg mens vriendelijke kat makkelijk`)
+
+})
+
+$('#desc__back').click(function() {
+    $('.chat__input_big').attr('style', 'display: none');
+
+    $('#bot_msg_3').attr('style', 'display: flex');
+    $('.chat__left_buttons').attr('style', 'display: flex');
+})
+
+$('.chat__cat_send_1').click(function() {
+    catDesc = $('#cat_desc').val();
+    document.cookie = "catDesc=" + catDesc;
+    $('.chat__cat_desc').html('<span>kattenradar </span>' + catDesc)
+    $('#user_msg_5_1').attr('style', 'display: flex')
+    $('#user_msg_5_1').addClass('user__msg_show');
+    $('#user_msg_5_1').find('p').text(catDesc)
+    $('.chat__input_big').attr('style', 'display:none')
+
+    setTimeout(() => {
+        $('#bot_msg_4').attr('style', 'display: flex')
+        $('#bot_msg_4').addClass('chat__msg_bot')
+        $('.chat__left_input_email').attr('style', 'display: block')
+    }, 500);
+})
+
+
+$('#email__back').click(function() {
+
+    $('#bot_msg_4').attr('style', 'display: none')
+    $('#bot_msg_4').removeClass('chat__msg_bot')
+    $('.chat__left_input_email').attr('style', 'display: none')
+
+    $('#user_msg_5_1').attr('style', 'display: none')
+    $('#user_msg_5_2').attr('style', 'display: none')
+    $('#bot_msg_3').attr('style', 'display: none')
+    $('#user_msg_4').attr('style', 'display: none')
+
+    $('#user_msg_3').attr('style', 'display: flex')
+    $('#user_msg_3').addClass('user__msg_show')
+})
+
+$('.email__cat_send').click(function() {
+    emailSend()
+})
+
+function emailSend() {
+    userEmail = $('#email').val();
+    document.cookie = "userEmail=" + userEmail;
+    $('#user_msg_6').find('p').text(userEmail);
+    $('#user_msg_6').attr('style', 'display: flex');
+    $('#user_msg_6').addClass('user__msg_show');
+    $('.chat__left_input_email').attr('style', 'display: none')
+
+    setTimeout(() => {
+        $('#bot_msg_5').attr('style', 'display: flex');
+        $('#bot_msg_5').addClass('chat__msg_bot')
+        $('#bot_msg_5').find('p > span').text(userEmail)
+        $('.chat__left_buttons_email').attr('style', 'display: flex')
+    }, 500);
+}
+
+$('#email').keypress(function(e) {
+    if (e.which == 13) {
+        emailSend()
+        return false; //prevent duplicate submission
+    }
+});
+
+$('.chat__left_e_error').click(function() {
+    $('.chat__left_buttons_email').attr('style', 'display: none')
+    $('#bot_msg_4').attr('style', 'display: flex')
+    $('#bot_msg_4').addClass('chat__msg_bot')
+    $('.chat__left_input_email').attr('style', 'display: block')
+})
+
+
+$('.chat__left_e_done').click(function() {
+    $('.chat__left_buttons_email').attr('style', 'display: none')
+    $('#user_msg_7').attr('style', 'display: flex')
+    $('#user_msg_7').addClass('user__msg_show');
+
+    setTimeout(() => {
+
+        $('#bot_msg_7').find('p > span').text(catName)
+        $('#bot_msg_7').attr('style', 'display: flex')
+        $('#bot_msg_7').addClass('chat__msg_bot')
+
+        $('.chat__left_buttons_notif').attr('style', 'display: flex')
+    }, 500);
+})
+
+$('.chat__left_n_error').click(function() {
+    //
+})
+
+$('#mob__back').click(function() {
+    $('#user_msg_9').attr('style', 'display: none')
+    $('#bot_msg_8').attr('style', 'display: none')
+    $('#user_msg_8').attr('style', 'display: none')
+    $('#bot_msg_7').attr('style', 'display: none')
+    $('#user_msg_7').attr('style', 'display: none')
+    $('#bot_msg_5').attr('style', 'display: none')
+    $('#user_msg_6').attr('style', 'display: none')
+    $('.chat__left_input_email').attr('style', 'display: flex')
+})
+
+$('.chat__left_n_done').click(function() {
+    $('#user_msg_8').attr('style', 'display: flex')
+    $('#user_msg_8').addClass('user__msg_show');
+
+    setTimeout(() => {
+        $('.chat__left_buttons_notif').attr('style', 'display: none')
+        $('#bot_msg_8').attr('style', 'display: flex')
+        $('#bot_msg_8').addClass('chat__msg_bot')
+        $('#user_msg_9').attr('style', 'display: flex')
+        $('#user_msg_9').addClass('user__msg_show');
+    }, 500);
+})
+
+var userNumber;
+
+$('#number__send').click(function() {
+    phoneSend()
+})
+
+function phoneSend() {
+    userNumber = $('#phone').val();
+    document.cookie = "userNumber=" + userNumber;
+    document.cookie = "getUpdate=" + true;
+    setTimeout(() => {
+        $('#bot_msg_9').find('p > span').text(userNumber)
+        $('#bot_msg_9').attr('style', 'display: flex');
+        $('#bot_msg_9').addClass('chat__msg_bot')
+
+        $('.chat__left_buttons_phone').attr('style', 'display: flex');
+
+    }, 500);
+}
+
+$('#phone').keypress(function(e) {
+    if (e.which == 13) {
+        phoneSend()
+        return false; //prevent duplicate submission
+    }
+});
+
+$('.chat__left_p_done').click(function() {
+    $('#user_msg_10').attr('style', 'display: flex')
+    $('#user_msg_10').addClass('user__msg_show');
+    $('.chat__left_buttons_phone').attr('style', 'display: none');
+
+    setTimeout(() => {
+
+        $('#bot_msg_10').attr('style', 'display: flex')
+            //    $('#bot_msg_10').find('p > span').text(CatAdress);
+        $('#bot_msg_10').addClass('chat__msg_bot')
+
+        $('.chat__left_final').attr('style', 'display: flex');
+
+    }, 500);
+})
+
+$('.chat__left_b_error').click(function() {
+    $('#user_msg_5_2').attr('style', 'display: flex')
+    $('#user_msg_5_2').addClass('user__msg_show');
+
+    $('.chat__left_buttons').attr('style', 'display: none')
+
+    setTimeout(() => {
+        $('#bot_msg_4').attr('style', 'display: flex')
+        $('#bot_msg_4').addClass('chat__msg_bot')
+        $('.chat__left_input_email').attr('style', 'display: block')
+
+        document.cookie = "steps=2";
+    }, 500);
+})
+
+$('.chat__left_n_error').click(function() {
+    $('.chat__left_buttons_notif').attr('style', 'display:none')
+
+    $('#bot_msg_10').attr('style', 'display: flex')
+    $('#bot_msg_10').addClass('chat__msg_bot')
+
+    $('.chat__left_final').attr('style', 'display: flex');
+
+    document.cookie = "steps=2";
+})
